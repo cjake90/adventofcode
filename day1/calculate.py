cc1f0ac789850e6acefe4b38f6f377e650874215
cjake90 <45885503+cjake90@users.noreply.github.com>
2018-12-15 18:54:14 +0100

    Update calculate.py
    Updated calculate.py to process the frequency changes and calculate the final frequency.

--- day1/calculate.py
+++ day1/calculate.py
@@ -1,5 +1,10 @@
-//import the frequency changes
-
+#import the list of frequency changes and create frequency int
 input_file = open("input.txt", "r")
-freq_changes = inputfile.readlines()
-print freq_changes
+freq_changes = input_file.readlines()
+frequency = 0
+
+#sum frequency changes and print the result
+for index in range(len(freq_changes)):
+    frequency += int(freq_changes[index])
+
+print(frequency)
